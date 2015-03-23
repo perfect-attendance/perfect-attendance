e6afd62d7dc34f269ee6d23e7c3eafeb3ffb1cad
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2015-03-24 01:26:54 +0800

    Flex box for the UI, also added guidelines.

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,22 @@
+/**
+ * General Guidelines:
+ * - We will be using ECMAScript5 syntax throughout this application.
+ *
+ * - Coding Guidelines, we should generally follow:
+ *   http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml
+ *
+ * - Remember to always enclose your code in a closure to avoid pollution of
+ *   the global scope.
+ *
+ */
+
 (function () {
-  document.write('<p>CHB and friends!</p>');
+  'use strict';
+
+  function addHello(element) {
+    element.innerHTML = '<h1>Hello World</h1>' + element.innerHTML;
+  }
+
+  var mainContent = document.querySelector('#main-content');
+  addHello(mainContent);
 })();
