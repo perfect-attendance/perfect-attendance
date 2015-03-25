5f5fcca8a19986ade4b56ed512ba11665cb82728
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2015-03-25 14:15:30 +0800

    Started work on home module in accordance to the design specification.

--- scripts/controllers/home.js
+++ scripts/controllers/home.js
@@ -5,8 +5,5 @@
 
   // expose the controller to the global scope
   PA.homeCtrl = function () {
-    var myParagraph = document.createElement('p');
-    myParagraph.innerHTML = 'This is a paragraph that is generated from the homeCtrl!';
-    this.mainView.appendChild(myParagraph);
   };
 })();
