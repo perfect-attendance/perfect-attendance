88aa4410498f7dbbd0cfca11034a39ab12f5947a
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2015-03-25 12:16:12 +0800

    Models and Views are now functional, hash baning also enabled. Next job, Controllersgit --no-pager add --all

--- scripts/app.js
+++ scripts/app.js
@@ -13,12 +13,43 @@
   'use strict';
 
   var Module = PA.Module;
+  // also modifiles window.PA.mods;
+
+  function renderModuleInLocation() {
+    var hash = window.location.hash;
+    var pathname = window.location.pathname;
+    var mods = window.PA.mods;
+    var found = false;
+    var renderMod = function (mod) {
+      found = true;
+      mod.render();
+    };
+    mods.forEach(function (mod) {
+      if (!hash) {
+        if (pathname == mod.pathname) {
+          renderMod(mod);
+        }
+      } else {
+        if (hash == mod.hash) {
+          renderMod(mod);
+        }
+      }
+    });
+    if (!found) {
+      var notFound = new Module('Not Found', '/notfound', 'views/notfound.html', false);
+      notFound.render();
+    }
+  };
 
   // initialize menu
-  var home = new Module('Home', 'This is the home view.');
-  var rooms = new Module('Rooms', 'This is the rooms view.');
-  var instructors = new Module('Instructors', 'This is the instructors view.');
-  var downloads = new Module('Downloads', 'This is the downloads view.');
-  var about = new Module('About', 'This is the about view.');
-  home.display();
+  var home = new Module('Home', '/', 'views/home.html');
+  var rooms = new Module('Rooms', '/rooms', 'views/rooms.html');
+  var instructors = new Module('Instructors', '/instructors', 'views/instructors.html');
+  var downloads = new Module('Downloads', '/downloads', 'views/downloads.html');
+  var about = new Module('About', '/about', 'views/about.html');
+  var mods = [home, rooms, instructors, downloads, about];
+  PA.mods = mods;
+
+  renderModuleInLocation();
+  window.addEventListener('hashchange', renderModuleInLocation);
 })();
--- scripts/models/Module.js
+++ scripts/models/Module.js
@@ -2,35 +2,67 @@
   'use strict';
 
   window.PA.Module = Module; // export to the global scope
+  // also modifies window.location.pathname
 
-  function Module(name, main) {
-    // display the Module in the DOM upon creation
-    var ul = document.querySelector('aside > ul');
-    var li = document.createElement('li');
-    li.innerHTML = name;
-    ul.appendChild(li);
+  var PA = window.PA;
+  var currentModule = PA.currentModule;
 
+  function Module(name, pathname, viewUrl, attachToMenu) {
     this.name = name;
-    this.main = main;
-    this.view = li;
-    this.displayOnClick(true);
+    this.pathname = pathname;
+    this.hash = '#' + pathname;
+    this.viewUrl = viewUrl;
+    if (attachToMenu !== false) {
+      this.attachToMenu();
+      this.renderOnClick(true);
+    }
   }
 
-  Module.prototype.display = function () {
-    var main = document.querySelector('#main-content');
-    main.innerHTML = this.main;
+  Module.prototype.render = function () {
+    var hash = window.location.hash;
+    if (hash) {
+      window.location.href = window.location.href.replace(hash, this.hash);
+    }
+    window.location.hash = this.hash;
+    currentModule = this;
+    if (!this.fetched) {
+      // AJAX the view
+      var thisModule = this;
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', thisModule.viewUrl, true);
+      xhr.responseType = 'text';
+      xhr.onload = function (e) {
+        thisModule.main = this.response;
+        var main = document.querySelector('#main-content');
+        main.innerHTML = thisModule.main;
+        thisModule.fetched = true;
+      };
+      xhr.send();
+    } else {
+      var main = document.querySelector('#main-content');
+      main.innerHTML = this.main;
+    }
   };
 
-  Module.prototype.displayOnClick = function (displayFlag) {
+  Module.prototype.renderOnClick = function (renderFlag) {
     var thisModule = this;
-    this.displayOnClick = function () {
-      thisModule.display();
+    this.renderOnClick = function () {
+      thisModule.render();
     };
-    if (displayFlag) {
-      this.view.addEventListener('click', this.displayOnClick);
+    if (renderFlag) {
+      this.view.addEventListener('click', this.renderOnClick);
     } else {
-      this.view.removeEventListener('click', this.displayOnClick);
+      this.view.removeEventListener('click', this.renderOnClick);
     }
-    return this.displayOnClick;
+    return this.renderOnClick;
+  };
+
+  Module.prototype.attachToMenu = function () {
+    // render the Module in the DOM upon creation
+    var ul = document.querySelector('aside > ul');
+    var li = document.createElement('li');
+    li.innerHTML = this.name;
+    ul.appendChild(li);
+    this.view = li;
   };
 })();
