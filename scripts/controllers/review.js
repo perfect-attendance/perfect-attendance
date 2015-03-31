961bd49e362b2df3b7be6076834b4901ca29bdb8
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2015-03-31 15:58:24 +0800

    Some edits, reviews...

--- scripts/controllers/review.js
+++ scripts/controllers/review.js
@@ -6,8 +6,5 @@
     // imports
     var PA = window.PA;
 
-    var myParagraph = document.createElement('p');
-    myParagraph.innerHTML = 'This is a paragraph that is generated from the reviewCtrl!';
-    this.mainView.appendChild(myParagraph);
   };
 })();
